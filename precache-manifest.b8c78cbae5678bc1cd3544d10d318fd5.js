e6304cb1d919e4822e66d06b0e831d7cf730024a
unknown <expireddata@gmail.com>
2020-05-26 22:38:25 +0100

    Updates

---
+++ precache-manifest.b8c78cbae5678bc1cd3544d10d318fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4445f968f5a3e91bfd41211e357c935f",
+    "url": "/TelosStreaks/index.html"
+  },
+  {
+    "revision": "7e171be5fb4243f99982",
+    "url": "/TelosStreaks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba8b3cb758109a1c9706",
+    "url": "/TelosStreaks/static/js/2.bf5c627d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TelosStreaks/static/js/2.bf5c627d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e171be5fb4243f99982",
+    "url": "/TelosStreaks/static/js/main.cd0ed1af.chunk.js"
+  },
+  {
+    "revision": "a2fde8f2b0a88a44e999",
+    "url": "/TelosStreaks/static/js/runtime-main.e9e9324b.js"
+  }
+]);
